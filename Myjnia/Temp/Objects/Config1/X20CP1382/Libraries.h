e54b211d92bff2b9973e21922b2ef94225720467
putzio <krzys.p9@gmail.com>
2021-06-01 23:20:50 +0200

    visu update

--- Myjnia/Temp/Objects/Config1/X20CP1382/Libraries.h
+++ Myjnia/Temp/Objects/Config1/X20CP1382/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20210601115419_
-#define _LIBRARIES_20210601115419_
+#ifndef _LIBRARIES_20210601214452_
+#define _LIBRARIES_20210601214452_
 
 __asm__(".section \".plc\"");
 
@@ -461,5 +461,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/AsIecCon/AsIecCon.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20210601115419_ */
+#endif /* _LIBRARIES_20210601214452_ */
 
